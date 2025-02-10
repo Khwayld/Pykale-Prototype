7177ad7ec34213a865e15096aa84283a4f02e66d
Khalid Al Babtain <khaledbabtain44@gmail.com>
2025-02-10 12:41:49 +0000

    added search and categorization

--- helpers/constants.py
+++ helpers/constants.py
@@ -9,41 +9,47 @@ EXAMPLES = [
         "name": "Video Loading Example",
         "description": "Load frames from short clips and display them in Streamlit for quick analysis.",
         "image": "",
-        "nav": "video_example"
+        "nav": "video_example",
+        "category": "Video Processing"
     },
     
     {
         "name": "Domain Adaptation Example",
         "description": "Learn to adapt models across related domains for more robust predictions.",
         "image": "",
-        "nav": "domain_adaptation"
+        "nav": "domain_adaptation",
+        "category": "Domain Adaptation",
     },
 
     {
         "name": "Third Example",
         "description": "Some Description",
         "image": "",
-        "nav": "domain_adaptation"
+        "nav": "domain_adaptation",
+        "category": "Other Transfer Learning"
     },
 
     {
         "name": "Fourth Example",
         "description": "Some Description",
         "image": "",
-        "nav": "domain_adaptation"
+        "nav": "domain_adaptation",
+        "category": "Multi-Modal"
     },
 
     {
         "name": "Fifth Example",
         "description": "Some Description",
         "image": "",
-        "nav": "domain_adaptation"
+        "nav": "domain_adaptation",
+        "category": "Domain Adaptation"
     },
 
     {
         "name": "Sixth Example",
         "description": "Some Description",
         "image": "",
-        "nav": "domain_adaptation"
+        "nav": "domain_adaptation",
+        "category": "Video Processing"
     }
 ]
